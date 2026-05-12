846fb67cca51099d51f9495cde68a3c1ad8098ef
Camden Beightler <camdenbeightler@gmail.com>
2026-05-11 21:01:22 -0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -119,7 +119,7 @@ export default function Hero() {
 
       {/* Resume Button - Float up on hover */}
       <motion.a
-        href="/Camden Beightler - Resume (2026) (LinkedIn).pdf"
+        href="/Camden Beightler - Resume (2025).pdf"
         download="Camden-Beightler-Resume.pdf"
         whileHover={{
           y: -6,
